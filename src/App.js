d2d8e86ef4a8620c9478738f273af258c3cc6ab4
Manjunatha Sai Uppu <manjunatha16.512@gmail.com>
2022-08-27 07:12:25 +0530

    lazy loading first changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+const FirstComponent = lazy(() => import('./components/FirstComponent'));
+const SecondComponent = lazy(() => import('./components/SecondComponent'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense
+      fallback={
+        <div>
+          <p>I am Loading</p>
+        </div>
+      }
+    >
+      <Router>
+        <Routes>
+          <Route path='/firstcomponent' element={<FirstComponent />} />
+          <Route path='/secondcomponent' element={<SecondComponent />} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
